2df5ebbacf5f82ec867b8f89906d9f2e71d58be4
AlfaHaker <alirefaatfayezismyname@gmail.com>
2026-08-14 20:12:08 +0300

    refactor: prune extra image wallpapers and set default stylix image

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -110,13 +110,7 @@
   # This will set your color palette
   # Default background
   # Add new images to ~/zaneyos/wallpapers
-  #stylixImage = ../../wallpapers/mountainscapedark.jpg;
-  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
-  #stylixImage = ../../wallpapers/Anime-Purple-eyes.png;
-  #stylixImage = ../../wallpapers/Rainnight.jpg;
-  #stylixImage = ../../wallpapers/zaney-wallpaper.jpg;
-  #stylixImage = ../../wallpapers/nix-wallpaper-stripes-logo.png;
-  #stylixImage = ../../wallpapers/beautifulmountainscape.jpg;
+  stylixImage = ../../wallpapers/beautifulmountainscape.jpg;
 
   # Set Waybar
   #  Available Options:
